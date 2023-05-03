f7add1e1ca58b0dd3989fe66510d28e4bb910942
aidenwaring <aiden.waring@icloud.com>
2023-05-03 14:42:17 +1000

    First test

---
+++ src/test/java/aidenwaring/intro/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package aidenwaring.intro.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    public void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}

4d39a5999200d2c36f3cc2521dbb9e312af256ec
aidenwaring <aiden.waring@icloud.com>
2023-05-25 13:52:19 +1000

    Testing for example get

--- src/test/java/aidenwaring/intro/cashcard/CashCardApplicationTests.java
+++ src/test/java/aidenwaring/intro/cashcard/CashCardApplicationTests.java
@@ -25,5 +25,15 @@ class CashCardApplicationTests {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // This converts the response String into a JSON-aware object with lots of helper methods.
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        // Store value of ID in JSON path to variable, assert it is not null
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(123.45);
     }
 }

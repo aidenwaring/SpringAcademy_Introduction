5e0735a842b83fb48645b52d84d4d131f9019b78
aidenwaring <aiden.waring@icloud.com>
2023-05-25 13:44:45 +1000

    Get mapping and test template

---
+++ src/main/java/aidenwaring/intro/cashcard/CashCardController.java
@@ -0,0 +1,18 @@
+package aidenwaring.intro.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<String> findById(){
+        return ResponseEntity.ok("{}");
+    }
+
+}
